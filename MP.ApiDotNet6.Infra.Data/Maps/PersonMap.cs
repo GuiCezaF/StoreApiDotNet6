8bde9cd5569870936cd2d1144353b0b5ec9e851c
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-05-11 13:51:42 -0300

    feat: Add DbContext and Mapper

---
+++ MP.ApiDotNet6.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MP.ApiDotNet6.Domain.Entities;
+
+namespace MP.ApiDotNet6.Infra.Data.Maps
+{
+    public class PersonMap : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("Pessoa");
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id).HasColumnName("idPessoa").UseIdentityColumn();
+            builder.Property(c => c.Document).HasColumnName("Documento");
+            builder.Property(c => c.Name).HasColumnName("Nome");
+            builder.Property(c => c.Phone).HasColumnName("Celular");
+
+            builder.HasMany(c => c.Purchases).WithOne(p => p.Person).HasForeignKey(c => c.PersonId);
+        }
+    }
+}
