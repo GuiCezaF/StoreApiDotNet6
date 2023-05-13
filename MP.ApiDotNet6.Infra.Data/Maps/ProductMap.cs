adc4da25a45c3b008f737f38c66a3e2d86989977
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-05-13 20:32:13 -0300

    feat: Add PurchaseMap and ProductMap

---
+++ MP.ApiDotNet6.Infra.Data/Maps/ProductMap.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MP.ApiDotNet6.Domain.Entities;
+
+namespace MP.ApiDotNet6.Infra.Data.Maps
+{
+    public class ProductMap : IEntityTypeConfiguration<Products>
+    {
+        public void Configure(EntityTypeBuilder<Products> builder)
+        {
+            builder.ToTable("Produto");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("IdProduto").UseIdentityColumn();
+            builder.Property(x => x.CodeErp).HasColumnName("CodErp");
+            builder.Property(x => x.Name).HasColumnName("Nome");
+            builder.Property(x => x.Price).HasColumnName("Preco");
+
+            builder.HasMany(x => x.Purchases)
+            .WithOne(p => p.Products)
+            .HasForeignKey(x => x.ProductId);
+        }
+    }
+}
---
+++ MP.ApiDotNet6.Infra.Data/Maps/PurchaseMap.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MP.ApiDotNet6.Domain.Entities;
+
+namespace MP.ApiDotNet6.Infra.Data.Maps
+{
+    public class PurchaseMap : IEntityTypeConfiguration<Purchase>
+    {
+        public void Configure(EntityTypeBuilder<Purchase> builder)
+        {
+            builder.ToTable("Compra");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("IdCompra").UseIdentityColumn();
+            builder.Property(x => x.PersonId).HasColumnName("IdPessoa");
+            builder.Property(x => x.ProductId).HasColumnName("IdProduto");
+            builder.Property(x => x.Date).HasColumnName("DataCompra");
+
+            builder.HasOne(x => x.Person).WithMany(x => x.Purchases);
+
+            builder.HasOne(x => x.Products).WithMany(x => x.Purchases);
+        }
+    }
+}
