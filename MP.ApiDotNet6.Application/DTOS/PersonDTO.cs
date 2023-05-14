70ae3da6cbe92140c18c6dbcdd18a7b23da8f2bb
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-05-14 20:30:50 -0300

    feat: Add DTOS and DTO Validator

---
+++ MP.ApiDotNet6.Application/DTOS/PersonDTO.cs
@@ -0,0 +1,10 @@
+namespace MP.ApiDotNet6.Application.DTOS
+{
+    public class PersonDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ MP.ApiDotNet6.Application/DTOS/Validations/PersonDTOValidator.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+
+namespace MP.ApiDotNet6.Application.DTOS.Validations
+{
+    public class PersonDTOValidator : AbstractValidator<PersonDTO>
+    {
+        public PersonDTOValidator()
+        {
+            RuleFor(x => x.Document)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Documento deve ser Informado");
+
+            RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Nome deve ser Informado");
+
+            RuleFor(x => x.Phone)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Telefone deve ser Informado");
+
+        }
+    }
+}
