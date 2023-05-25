c9f9474107808ca4b2c527f60faa3cff2f8a3d0f
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-05-25 12:01:36 -0300

    feat: add product DTO

---
+++ MP.ApiDotNet6.Application/DTOS/ProductDTO.cs
@@ -0,0 +1,11 @@
+namespace MP.ApiDotNet6.Application.DTOS
+{
+  public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CodErp { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ MP.ApiDotNet6.Application/DTOS/Validations/ProductDTOValidator.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+
+namespace MP.ApiDotNet6.Application.DTOS.Validations
+{
+  public class ProductDTOValidator : AbstractValidator<ProductDTO>
+    {
+        public ProductDTOValidator()
+        {
+            RuleFor(x => x.CodErp)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("CodErp Deve ser informado");
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Nome deve ser informado");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("Preço deve ser maior que Zero");
+        }
+    }
+}
