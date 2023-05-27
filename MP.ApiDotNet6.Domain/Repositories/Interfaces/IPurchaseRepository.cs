6316bc59f2f2ba3cf0952d41a3ed8221a748452e
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-05-27 14:27:58 -0300

    chore: add purchase repository

---
+++ MP.ApiDotNet6.Domain/Repositories/Interfaces/IPurchaseRepository.cs
@@ -0,0 +1,15 @@
+using MP.ApiDotNet6.Domain.Entities;
+
+namespace MP.ApiDotNet6.Domain.Repositories.Interfaces
+{
+  public interface IPurchaseRepository
+    {
+        Task<Purchase> GetByIdAsync(int id);
+		Task<ICollection<Purchase>> GetAllAsync();
+		Task<Purchase> CreateAsync(Purchase purchase);
+		Task EditAsync(Purchase purchase);
+		Task DeleteAsync(Purchase purchase);
+		Task<ICollection<Purchase>> GetByPersonIdAsync(int personId);
+		Task<ICollection<Purchase>> GetByProductIdAsync(int productId);
+    }
+}
